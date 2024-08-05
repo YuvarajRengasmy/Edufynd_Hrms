4eaa738cdf1d5fadf798f0fa40b6bf85d5b80449
Yuvaraj R <yuvarajr998@gmail.com>
2024-08-05 15:28:45 -0700

    inite

--- src/Pages/Superadmin/Payroll.js
+++ src/Pages/Superadmin/Payroll.js
@@ -1,12 +1,105 @@
-import React from "react";
-import Navbar from "../../Components/Navbar";
-import SuperadminSidebar from "../../Components/SuperadminSidebar";
-import Profile from "../../Assests/Images/Profile.jpg";
+import React, { useEffect, useState, useRef } from "react";
+import Sortable from 'sortablejs';
+import {getallStaff,deleteStaff} from "../../Api/SuperAdmin/Employees";
 import { Link } from "react-router-dom";
-import { FaCameraRotate } from "react-icons/fa6";
-import "bootstrap/dist/css/bootstrap.min.css";
+import SuperAdminSidebar from "../../Components/SuperadminSidebar";
+import Navbar from "../../Components/Navbar";
+// import { formatDate } from "../../../Utils/DateFormat";
+import { FaFilter } from "react-icons/fa";
+import { toast } from "react-toastify";
 
-export const Payroll = () => {
+import { Dialog, DialogContent, DialogTitle, IconButton, Pagination, backdropClasses, radioClasses, } from "@mui/material";
+
+export const ListEmployees = () => {
+
+
+
+
+
+  
+  const [staff, setStaff] = useState();
+  const [open, setOpen] = useState(false);
+  const [deleteId, setDeleteId] = useState();
+  const pageSize = 10;
+  const [pagination, setPagination] = useState({
+    count: 0,
+    from: 0,
+    to: 0
+  });
+
+  useEffect(() => {
+    getAllStaffDetails();
+  }, [pagination.from, pagination.to]);
+
+  const getAllStaffDetails = () => {
+    const data = {
+      limit: 10,
+      page: pagination.from,
+    };
+
+    getallStaff(data)
+      .then((res) => {
+       
+        setStaff(res?.data?.result);
+        setPagination({ ...pagination, count: res?.data?.result?.staffCount });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const openPopup = (data) => {
+    setOpen(true);
+    setDeleteId(data);
+  };
+
+  const closePopup = () => {
+    setOpen(false);
+  };
+  const handlePageChange = (event, page) => {
+    const from = (page - 1) * pageSize;
+    const to = (page - 1) * pageSize + pageSize;
+    setPagination({ ...pagination, from: from, to: to });
+  };
+  const deleteStaffData = () => {
+    deleteStaff (deleteId)
+      .then((res) => {
+        toast.success(res?.data?.message);
+        closePopup();
+        getAllStaffDetails();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    const table = tableRef.current;
+
+    // Apply SortableJS to the table headers
+    const sortable = new Sortable(table.querySelector('thead tr'), {
+      animation: 150,
+      swapThreshold: 0.5,
+      handle: '.sortable-handle',
+      onEnd: (evt) => {
+        const oldIndex = evt.oldIndex;
+        const newIndex = evt.newIndex;
+
+        // Move the columns in the tbody
+        table.querySelectorAll('tbody tr').forEach((row) => {
+          const cells = Array.from(row.children);
+          row.insertBefore(cells[oldIndex], cells[newIndex]);
+        });
+      }
+    });
+
+    return () => {
+      sortable.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -14,210 +107,155 @@ export const Payroll = () => {
       <br />
       <br />
       <br />
-      <div className="container-fluid" style={{ fontSize: "14px" }}>
+
+      <div className="container-fluid">
         <div className="row">
           <div className="col-lg-3">
-            <SuperadminSidebar />
+            <SuperAdminSidebar />
           </div>
           <div className="col-lg-9">
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-md-5">
-                  <div className="card mb-4 border-0">
-                    <div className="card-body p-4">
-                      <div className="card border-0 mb-0">
-                        <div className="row g-0">
-                          <div className="col-md-3">
-                            <div className="position-relative">
-                              <img
-                                src={Profile}
-                                className="img-fluid rounded-circle"
-                                style={{ width: "5rem", height: "5rem" }}
-                                alt="profile_image"
-                              />
-                              <div className="position-absolute bottom-0 end-0">
-                                <FaCameraRotate />
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-8">
-                            <div className="card-body ms-4">
-                              <h5 className="card-title mb-0">John Doe</h5>
-                              <p className="card-text mb-1">Developer</p>
-                              <p className="text-bg-success d-inline px-3 py-1 text-capitalize fw-semibold rounded-1">
-                                <small>Active</small>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="list-group list-group-flush">
-                        <Link
-                          to="#"
-                          className="list-group-item list-group-item-action"
-                        >
-                          Manager
-                        </Link>
-                        <Link
-                          to="#"
-                          className="list-group-item list-group-item-action"
-                        >
-                          Email
-                        </Link>
-                        <Link
-                          to="#"
-                          className="list-group-item list-group-item-action"
-                        >
-                          Employee Information
-                        </Link>
-                        <Link
-                          to="#"
-                          className="list-group-item list-group-item-action"
-                        >
-                          Documents
-                        </Link>
-                        <Link
-                          to="#"
-                          className="list-group-item list-group-item-action"
-                        >
-                          Change Password
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="card border-0 mb-3">
-                    <div className="card-header bg-white">
-                      <h6 className="h6 fw-semibold text-capitalize float-start">
-                        Total Salary Details
-                      </h6>
-                    </div>
-                    <div className="card-body p-4">
-                      <form>
-                        <div className="row mb-3">
-                          <div className="col-6 fw-bold">
-                            <i className="fas fa-id-badge"></i> Gross Salary:
-                          </div>
-                          <div className="col-6">18,000</div>
-                        </div>
-                        <div className="row mb-3">
-                          <div className="col-6 fw-bold">
-                            <i className="fas fa-id-badge"></i> Total
-                            Deductions:
-                          </div>
-                          <div className="col-6">500</div>
-                        </div>
-                        <div className="row mb-3">
-                          <div className="col-6 fw-bold">
-                            <i className="fas fa-id-badge"></i> Net Salary:
-                          </div>
-                          <div className="col-6">17,500</div>
-                        </div>
-                        <button
-                          className="btn btn-sm text-capitalize fw-semibold px-3 py-1 float-end ms-3"
-                          style={{ backgroundColor: "#7267ef", color: "#fff" }}
-                        >
-                          Update
-                        </button>
-                        <button
-                          className="btn btn-sm text-capitalize fw-semibold px-3 py-1 float-end"
-                          style={{ backgroundColor: "#231f20", color: "#fff" }}
-                        >
-                          Cancel
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-7">
-                  <div className="card border-0 mb-3">
-                    <div className="card-header bg-white">
-                      <h6 className="h6 fw-semibold text-capitalize float-start">
-                        Allowances
-                      </h6>
-                    </div>
-                    <div className="card-body p-4">
-                      <form>
-                        <div className="mb-3">
-                          <label className="form-label">
-                            House Rent Allowances
-                          </label>
-                          <input
-                            type="text"
-                            className="form-control rounded-1"
-                            placeholder="Example 2500"
-                            style={{ fontSize: "12px" }}
-                          />
-                        </div>
-                        <div className="mb-3">
-                          <label className="form-label">Conveyance</label>
-                          <input
-                            type="text"
-                            className="form-control rounded-1"
-                            placeholder="Example 2500"
-                            style={{ fontSize: "12px" }}
-                          />
-                        </div>
-                        <div className="mb-3">
-                          <label className="form-label">Other Allowances</label>
-                          <input
-                            type="text"
-                            className="form-control rounded-1"
-                            placeholder="Example 2500"
-                            style={{ fontSize: "12px" }}
-                          />
-                        </div>
-                        <button
-                          className="btn btn-sm fw-semibold text-capitalize text-white float-end px-4 py-1"
-                          style={{ backgroundColor: "#7267ef" }}
-                        >
-                          <i className="fas fa-plus-circle"></i>&nbsp;&nbsp;Add
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                  <div className="card border-0 mb-3">
-                    <div className="card-header bg-white">
-                      <h6 className="h6 fw-semibold text-capitalize float-start">
-                        Deductions
-                      </h6>
-                    </div>
-                    <div className="card-body p-4">
-                      <form>
-                        <div className="mb-3">
-                          <label className="form-label">Provident Fund</label>
-                          <input
-                            type="text"
-                            className="form-control rounded-1"
-                            placeholder="Example 2500"
-                            style={{ fontSize: "12px" }}
-                          />
-                        </div>
-                        <div className="mb-3">
-                          <label className="form-label">Tax Deductions</label>
-                          <input
-                            type="text"
-                            className="form-control rounded-1"
-                            placeholder="Example 2500"
-                            style={{ fontSize: "12px" }}
-                          />
-                        </div>
-                        <button
-                          className="btn btn-sm fw-semibold text-capitalize text-white float-end px-4 py-1"
-                          style={{ backgroundColor: "#7267ef" }}
-                        >
-                          <i className="fas fa-plus-circle"></i>&nbsp;&nbsp;Add
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
+            <section className="d-flex justify-content-between align-items-center mb-4">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                    <Link
+                      to="/SADashboard"
+                      className="text-dark text-decoration-none"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Employees
+                  </li>
+                </ol>
+              </nav>
+              <Link
+                to="/"
+                className="btn"
+                style={{ backgroundColor: "#7267ef", color: "#fff" }}
+              >
+                Log Out
+              </Link>
+            </section>
+            <div className="card border-0 p-2">
+              <div className="card-header border-0 bg-white">
+                <h6 className="h6 fw-semibold float-start">PayRoll Report</h6>
+                
               </div>
+              <div className="content-body">
+            <div className="container">
+            <div className="row">
+          <div className="col-xl-12">
+          <div className="col-md-12">
+            <div className="card rounded-0 mt-2 border-0">
+              <div className="card-body">
+                <div className="card-table">
+                  <div className="table-responsive">
+                    <table className=" table table-hover card-table dataTable table-responsive-sm text-center"   style={{ color: '#9265cc', fontSize: '13px' }}
+              ref={tableRef}>
+                      <thead className="table-light">
+                        <tr style={{fontFamily: "Plus Jakarta Sans", fontSize: "12px" }}>
+                          <th className="text-capitalize text-start sortable-handle"> S.No.</th>
+                          <th className="text-capitalize text-start sortable-handle">Emp_ID </th>
+                          <th className="text-capitalize text-start sortable-handle"> DOJ </th>
+                          <th className="text-capitalize text-start sortable-handle"> Name </th>
+                          <th className="text-capitalize text-start sortable-handle"> Designation</th>
+                          <th className="text-capitalize text-start sortable-handle"> Reporting_Manager </th>
+                          <th className="text-capitalize text-start sortable-handle"> Contact No </th>
+                          
+                          <th className="text-capitalize text-start sortable-handle"> Status </th>
+                          <th className="text-capitalize text-start sortable-handle"> Action </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      {staff?.map((data, index) => (
+                        <tr key={index} style={{ fontFamily: "Plus Jakarta Sans", fontSize: "11px" }} >
+                           <td className="text-capitalize text-start">{pagination.from + index + 1}</td>
+                          <td className="text-capitalize text-start">{data?.employeeID}</td>
+                          <td className="text-capitalize text-start">{data?.doj}</td>
+                          <td className="text-capitalize text-start">{data?.empName}</td>
+                          <td className="text-capitalize text-start">{data?.designation}</td>
+                          <td className="text-capitalize text-start">{data?.reportingManager}</td>
+                          <td className="text-capitalize text-start">{data?.mobileNumber}</td>
+                          
+                          <td className="text-capitalize text-start">{data?.status}</td>
+                          
+                          <td>
+                          <div className="d-flex">
+                               
+                            <i className="fa fa-credit-card" style={{fontSize: 24, color: 'red'}} />
+
+                             
+                               
+                              </div>
+                             
+                                </td>
+                        </tr>
+                      
+                      
+                    ))}
+                    {staff?.length === 0 ? (
+                     <tr>
+                       <td className="form-text text-danger" colSpan="9">
+                         No data In Staff
+                       </td>
+                     </tr>
+                    ) : null}
+                     
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+               
+              </div>
+              <div className="float-right my-2">
+                        <Pagination
+                          count={Math.ceil(pagination.count / pageSize)}
+                          onChange={handlePageChange}
+                          variant="outlined"
+                          shape="rounded"
+                          color="primary"
+                        />
+                      </div>
+            </div>
+          </div>
+          </div>
+        </div>
+            </div>
+          </div>
+              
             </div>
           </div>
         </div>
       </div>
+      <Dialog open={open}>
+        <DialogContent>
+          <div className="text-center m-4">
+            <h5 className="mb-4 text-capitalize">
+              Are you sure you want to Delete <br /> the selected Staff?
+            </h5>
+            <button
+              type="button"
+              className="btn btn-save btn-danger fw-semibold text-uppercase rounded-pill px-4 py-2 mx-3"
+              onClick={deleteStaffData}
+              style={{ fontFamily: 'Plus Jakarta Sans', fontSize: '12px' }}
+            >
+              Yes
+            </button>
+            <button
+              type="button"
+              className="btn btn-cancel btn-success fw-semibold text-uppercase rounded-pill px-4 py-2 "
+              onClick={closePopup}
+              style={{  fontFamily: 'Plus Jakarta Sans', fontSize: '12px' }}
+            >
+              No
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+                
     </>
   );
 };
-
-export default Payroll;
+export default ListEmployees;
