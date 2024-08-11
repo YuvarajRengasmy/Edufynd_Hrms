827fec51075ce6d4bd7fd8740523fe45cfce580e
Yuvaraj R <yuvarajr998@gmail.com>
2024-08-10 18:10:07 -0700

    inite

--- src/Pages/Superadmin/Employees/AddEmployees.js
+++ src/Pages/Superadmin/Employees/AddEmployees.js
@@ -33,7 +33,7 @@ export const AddStaff = () => {
     state: "",
     city: "",
     idCard: "",
-    status: "",
+    active: "",
     privileges: "",
     companyAssests: "",
     mobileName: "",
@@ -74,7 +74,7 @@ export const AddStaff = () => {
     state: { required: false },
     city: { required: false },
     idCard: { required: false }, // – Yes / No (If ‘Yes’ card to be generated)
-    status: { required: false },
+    active: { required: false },
     privileges: { required: false },
     companyAssests: { required: false },
     mobileName: { required: false },
@@ -177,8 +177,8 @@ export const AddStaff = () => {
       error.idCard.required = true;
     }
 
-    if (data.status === "") {
-      error.status.required = true;
+    if (data.active === "") {
+      error.active.required = true;
     }
     if (data.privileges === "") {
       error.privileges.required = true;
@@ -852,23 +852,23 @@ export const AddStaff = () => {
                             </div>
                             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
                               <label style={{ color: "#231F20" }}>
-                                Status <span className="text-danger">*</span>
+                                active <span className="text-danger">*</span>
                               </label>
                               <select
                                 className="form-select text-muted  rounded-1"
                                 onChange={handleInputs}
-                                name="status"
+                                name="active"
                                 style={{
                                   backgroundColor: "#fff",
                                   fontFamily: "Plus Jakarta Sans",
                                   fontSize: "12px",
                                 }}
                               >
-                                <option value="">Select Status Type</option>
+                                <option value="">Select active Type</option>
                                 <option value="Active">Active</option>
                                 <option value="Inactive">Inactive</option>
                               </select>
-                              {errors.status.required ? (
+                              {errors.active.required ? (
                                 <span className="form-text text-danger">
                                   This field is required.
                                 </span>
