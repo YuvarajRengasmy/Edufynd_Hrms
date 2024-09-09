0caff727f83355eb9eeb6771a38df3950e228c97
JayJo17 <gopinath172023@gmail.com>
2024-09-09 17:39:48 +0530

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,20 +6,18 @@ import { isAuthenticated } from '../../Utils/Auth';
 import { toast } from 'react-toastify';
 import { loginUser } from '../../Api/Login';
 import { Link } from "react-router-dom";
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaUser, FaLock, FaUserLock } from 'react-icons/fa';
 import Login_img from "../../Assests/Images/Login_img6.jpg";
-import { FaUser, FaLock, FaUserLock } from "react-icons/fa";
-
 
 export const Login = () => {
   const [inputs, setInputs] = useState({ email: "", password: "" });
   const [errors, setErrors] = useState({ email: { required: false, valid: false }, password: { required: false, valid: false } });
   const [submitted, setSubmitted] = useState(false);
-  const [passwordVisible, setPasswordVisible] = useState(false); // State for password visibility
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
 
   const handleValidation = (data) => {
-    let newErrors = {
+    return {
       email: {
         required: data.email === "",
         valid: !isValidEmail(data.email)
@@ -29,7 +27,6 @@ export const Login = () => {
         valid: !isValidPassword(data.password)
       }
     };
-    return newErrors;
   };
 
   const handleInputs = (event) => {
@@ -42,187 +39,196 @@ export const Login = () => {
   };
 
   const handleErrors = (obj) => {
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            const prop = obj[key];
-            if (prop.required === true || prop.valid === true) {
-                return false;
-            }
-        }
-    }
-    return true;
-  }
+    return !Object.values(obj).some(prop => prop.required || prop.valid);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newError = handleValidation(inputs);
-    setErrors(newError);
+    const newErrors = handleValidation(inputs);
+    setErrors(newErrors);
     setSubmitted(true);
-    if (handleErrors(newError)) {
-        loginUser(inputs).then(res => {
-            let token = res?.data?.result?.token;
-            let loginType = res?.data?.result?.loginType;
-           
-            if (loginType === 'superAdmin') {
-                let superAdminId = res?.data?.result?.superAdminDetails?._id;
-                let data = {
-                    token: token, superAdminId: superAdminId, loginType: loginType
-                };
-                saveToken(data);
-                if (isAuthenticated()) {
-                    navigate("/SADashboard");
-                    window.location.reload(); // Refresh the page
-                }
-            }
-           
-            if (loginType === 'staff') {
-              let staffId = res?.data?.result?.staffDetails?._id;
-              let data = {
-                  token: token, staffId: staffId, loginType: loginType
-              };
-              saveToken(data);
-              if (isAuthenticated()) {
-                  navigate("/StaffDashboard");
-                  window.location.reload(); // Refresh the page
-              }
+    if (handleErrors(newErrors)) {
+      loginUser(inputs).then(res => {
+        const { token, loginType } = res?.data?.result || {};
+        if (loginType === 'superAdmin') {
+          const superAdminId = res?.data?.result?.superAdminDetails?._id;
+          saveToken({ token, superAdminId, loginType });
+          if (isAuthenticated()) {
+            navigate("/SADashboard");
+            window.location.reload();
           }
-            toast.success(res?.data?.message);
-        })
-        .catch((err) => {
-            toast.error(err?.response?.data?.message);
-        });
+        } else if (loginType === 'staff') {
+          const staffId = res?.data?.result?.staffDetails?._id;
+          saveToken({ token, staffId, loginType });
+          if (isAuthenticated()) {
+            navigate("/StaffDashboard");
+            window.location.reload();
+          }
+        }
+        toast.success(res?.data?.message);
+      }).catch((err) => {
+        toast.error(err?.response?.data?.message);
+      });
     }
-  }
+  };
 
   if (isAuthenticated()) {
     const type = getLoginType();
-     if (type === 'superAdmin') { return <Navigate to="/SADashboard" /> }
-    else if (type === 'staff') { return <Navigate to="/StaffDashboard" /> }
-    else  { return <Navigate to="/" /> }
+    return <Navigate to={type === 'superAdmin' ? "/SADashboard" : type === 'staff' ? "/StaffDashboard" : "/"} />;
   }
-
-
-
-
 
   return (
     <>
-      <section className="container-fluid d-flex align-items-center justify-content-center min-vh-100 bg-primary">
-        <div className="row justify-content-center w-100">
-          <div className="col-12 col-md-8 col-lg-8">
-            <div className="card border-0 shadow-lg text-bg-whiite my-5">
-              <div className="row g-0">
-                <div className="col-md-6 d-none d-md-block">
-                  <img
-                    className="img-fluid rounded-start h-100 w-100"
-                    src={Login_img}
-                    alt="login_image"
-                  />
-                </div>
-                <div className="col-md-6 p-5">
-                  <h5 className="h5 fw-semibold text-dark text-start">
-                    Welcome to <span className="text-primary">Afynd</span>
-                  </h5>
-                  <p className="text-muted text-start">
-                    <small>Welcome back, please login to your account</small>
-                  </p>
-                  <form className="py-4" onSubmit={handleSubmit}>
-                    <div className="input-group mb-3">
-                      <span
-                        className="input-group-text rounded-start-1"
-                        id="basic-addon1"
-                      >
-                        <FaUser />
-                      </span>
-                      <input
+    <section className="container-fluid d-flex align-items-center justify-content-center min-vh-100 bg-primary">
+      <div className="row justify-content-center w-100">
+        <div className="col-12 col-md-8 col-lg-6">
+          <div className="card border-0 shadow-lg text-bg-white my-5">
+            <div className="row g-0">
+              <div className="col-md-6 d-none d-md-block">
+                <img className="img-fluid rounded-start h-100 w-100" src={Login_img} alt="login_image" />
+              </div>
+              <div className="col-md-6 p-5">
+                <h5 className="h5 fw-semibold text-dark text-start">
+                  Welcome to <span className="text-primary">Afynd</span>
+                </h5>
+                <p className="text-muted text-start">
+                  <small>Welcome back, please login to your account</small>
+                </p>
+                <form className="py-4" onSubmit={handleSubmit}>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text rounded-start">
+                      <FaUser />
+                    </span>
+                    <input
                       type="email"
                       name="email"
                       onChange={handleInputs}
                       className="form-control"
-                      id="exampleInputEmail"
-                      aria-describedby="emailHelp"
                       placeholder="Email Address..."
-                      style={{ fontSize: '12px' }}
                     />
-                      {errors.email.required ? (
-                    <div className="text-danger form-text">
-                      This field is required.
-                    </div>
-                  ) : errors.email.valid ? (
-                    <div className="text-danger form-text">
-                      Enter valid Email Id.
-                    </div>
-                  ) : null}
-                    </div>
-                    <div className="input-group mb-3">
-                      <span
-                        className="input-group-text rounded-start-1"
-                        id="basic-addon2"
-                      >
-                        <FaLock />
-                      </span>
-                      <input
+                    {errors.email.required && (
+                      <div className="text-danger form-text">
+                        This field is required.
+                      </div>
+                    )}
+                    {errors.email.valid && !errors.email.required && (
+                      <div className="text-danger form-text">
+                        Enter a valid Email Id.
+                      </div>
+                    )}
+                  </div>
+                  <div className="input-group mb-3 position-relative">
+                    <span className="input-group-text rounded-start">
+                      <FaLock />
+                    </span>
+                    <input
                       type={passwordVisible ? 'text' : 'password'}
                       name="password"
                       onChange={handleInputs}
                       autoComplete="off"
                       className="form-control rounded-end"
-                      id="exampleInputPassword"
                       placeholder="Password..."
-                      style={{ fontSize: '12px' }}
                     />
                     <button
                       type="button"
                       onClick={() => setPasswordVisible(!passwordVisible)}
-                      style={{
-                        position: 'absolute',
-                        right: '10px',
-                        top: '50%',
-                        transform: 'translateY(-50%)',
-                        background: 'transparent',
-                        border: 'none',
-                        cursor: 'pointer',
-                      }}
+                      className="btn btn-link position-absolute end-0 top-50 translate-middle-y"
                     >
                       {passwordVisible ? <FaEyeSlash /> : <FaEye />}
                     </button>
-                    {errors.password.required ? (
-                    <div className="text-danger form-text">
-                      This field is required.
-                    </div>
-                  ) : errors.password.valid ? (
-                    <div className="text-danger form-text">
-                      A minimum 8 characters password contains a <br />
-                      combination of {''}
-                      <strong>uppercase, lowercase, {''}</strong>
-                      <strong>special <br /> character{''}</strong> and <strong>number</strong>.
-                    </div>
-                  ) : null}
-                    </div>
-                    <div className="text-end">
-                      <Link
-                        to="#"
-                        className="text-decoration-none text-primary fw-semibold"
-                      >
-                        Forget Password?
-                      </Link>
-                    </div>
-                    <div className="text-end mt-3">
-                      <button
-                        type="submit" target='_self'
-                        className="btn btn-primary px-3 py-2 fw-semibold text-capitalize"
-                      >
-                        <FaUserLock /> &nbsp; Login
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                    {errors.password.required && (
+                      <div className="text-danger form-text">
+                        This field is required.
+                      </div>
+                    )}
+                    {errors.password.valid && !errors.password.required && (
+                      <div className="text-danger form-text">
+                        A minimum 8 characters password containing a combination of <strong>uppercase, lowercase, special characters, and numbers</strong>.
+                      </div>
+                    )}
+                  </div>
+                  <div className="text-end">
+                    <Link to="#" className="text-decoration-none text-primary fw-semibold">
+                      Forget Password?
+                    </Link>
+                  </div>
+                  <div className="text-end mt-3">
+                    <button
+                      type="submit"
+                      className="btn btn-primary px-3 py-2 fw-semibold text-capitalize"
+                    >
+                      <FaUserLock /> &nbsp; Login
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+   
+
+    <div className="container-fluid bg-body-tertiary ">
+  <div className="bg-holder bg-auth-card-overlay" style={{backgroundImage:`url(${require("../../Assests/Images/Login_img2.jpg")})`}} />
+  {/*/.bg-holder*/}
+  <div className="row flex-center position-relative min-vh-100 g-0 py-5">
+    <div className="col-11 col-sm-10 col-xl-8">
+      <div className="card border border-translucent auth-card">
+        <div className="card-body pe-md-0">
+          <div className="row align-items-center gx-0 gy-7">
+            <div className="col-5 bg-body-highlight dark__bg-gray-1100 rounded-3 position-relative overflow-hidden auth-title-box">
+              <div className="bg-holder position-relative" style={{backgroundImage:`url(${require("../../Assests/Images/Login_img6.jpg")})`}} />
+              {/*/.bg-holder*/}
+              <div className="position-relative px-4 px-lg-7 pt-7 pb-7 pb-sm-5 text-center text-md-start pb-lg-7 pb-md-7">
+                <h3 className="mb-3 text-body-emphasis fs-7">Phoenix Authentication</h3>
+                <p className="text-body-tertiary">Give yourself some hassle-free development process with the uniqueness of Phoenix!</p>
+                <ul className="list-unstyled mb-0 w-max-content w-md-auto">
+                  <li className="d-flex align-items-center"><span className="uil uil-check-circle text-success me-2" /><span className="text-body-tertiary fw-semibold">Fast</span></li>
+                  <li className="d-flex align-items-center"><span className="uil uil-check-circle text-success me-2" /><span className="text-body-tertiary fw-semibold">Simple</span></li>
+                  <li className="d-flex align-items-center"><span className="uil uil-check-circle text-success me-2" /><span className="text-body-tertiary fw-semibold">Responsive</span></li>
+                </ul>
+              </div>
+              <div className="position-relative z-n1 mb-6 d-none d-md-block text-center mt-md-15"><img className="auth-title-box-img d-dark-none" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s" alt /><img className="auth-title-box-img d-light-none" src="../../../assets/img/spot-illustrations/auth-dark.png" alt /></div>
+            </div>
+            <div className="col-7">
+              <div className="auth-form-box p-4">
+                <div className="text-center mb-7"><a className="d-flex flex-center text-decoration-none mb-4" href="../../../index.html">
+                    <div className="d-flex align-items-center fw-bolder  d-inline-block"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s" alt="phoenix" width={58} /></div>
+                  </a>
+                  <h3 className="text-body-highlight">Sign In</h3>
+                  <p className="text-body-tertiary">Get access to your account</p>
+                </div><button className="btn btn-phoenix-secondary w-100 mb-3"><span className="fab fa-google text-danger me-2 fs-9" />Sign in with google</button><button className="btn btn-phoenix-secondary w-100"><span className="fab fa-facebook text-primary me-2 fs-9" />Sign in with facebook</button>
+                <div className="position-relative">
+                  <hr className="bg-body-secondary mt-5 mb-4" />
+                  <div className="divider-content-center bg-body-emphasis">or use email</div>
+                </div>
+                <div className="mb-3 text-start"><label className="form-label" htmlFor="email">Email address</label>
+                  <div className="form-icon-container"><input className="form-control form-icon-input" id="email" type="email" placeholder="name@example.com" /><span className="fas fa-user text-body fs-9 form-icon" /></div>
+                </div>
+                <div className="mb-3 text-start"><label className="form-label" htmlFor="password">Password</label>
+                  <div className="form-icon-container" data-password="data-password"><input className="form-control form-icon-input pe-6" id="password" type="password" placeholder="Password" data-password-input="data-password-input" /><span className="fas fa-key text-body fs-9 form-icon" /><button className="btn px-3 py-0 h-100 position-absolute top-0 end-0 fs-7 text-body-tertiary" data-password-toggle="data-password-toggle"><span className="uil uil-eye show" /><span className="uil uil-eye-slash hide" /></button></div>
+                </div>
+                <div className="row flex-between-center mb-7">
+                  <div className="col-auto">
+                    <div className="form-check mb-0"><input className="form-check-input" id="basic-checkbox" type="checkbox" defaultChecked="checked" /><label className="form-check-label mb-0" htmlFor="basic-checkbox">Remember me</label></div>
+                  </div>
+                  <div className="col-auto"><a className="fs-9 fw-semibold" href="../../../pages/authentication/card/forgot-password.html">Forgot Password?</a></div>
+                </div><button className="btn btn-primary w-100 mb-3">Sign In</button>
+                <div className="text-center"><a className="fs-9 fw-bold" href="../../../pages/authentication/card/sign-up.html">Create an account</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+   
     </>
+    
   );
 };
 
